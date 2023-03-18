e7bce066bd4558fbcd6d5f0c126955243ea71947
Rustam Guseinov <rg20031991.com>
2023-03-18 13:03:43 +0300

    add's new components commit

---
+++ src/API.tsx
@@ -0,0 +1,9 @@
+
+export const API = () => {
+  return (
+    <>
+    const YandexMapAPI = "4eabad4d-4411-4694-b789-5b061a35aee2";
+    </>
+  )
+}
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { API } from './API';
 import './App.css';
+
 
 export function App() {
   return (
     <div className="App">
-          Learn React
+      <API/>
     </div>
+    
   );
 }
 
