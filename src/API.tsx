789dd96d4c5b6fecab146145c7338f4d72ba6298
Rustam Guseinov <rg20031991.com>
2023-03-18 14:30:43 +0300

    formmating commit

--- src/API.tsx
+++ src/API.tsx
@@ -1,12 +1,11 @@
 
   const YandexMapAPI = "4eabad4d-4411-4694-b789-5b061a35aee2";
 
-
 export const API = () => {
 
   return (
     <div>
-    {YandexMapAPI};
+      {YandexMapAPI};
     </div>
   )
 }
