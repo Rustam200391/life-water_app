506fb2259caaf8984da757d5c9de876af8fd0e4c
Rustam Guseinov <rg20031991.com>
2023-03-29 22:33:23 +0300

    intermedia commit

--- src/components/YMaps.jsx
+++ src/components/YMaps.jsx
@@ -1,20 +1,29 @@
 import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
 const center = [40.39, 49.83]; 
+// геокоординаты Баку
 
 const images = [...Array(16)].map((n,i) => {
+  // перебор массива с изображениями иконок
   const letter = String.fromCharCode(i +90);
+  //String.fromCharCode - возвращает строку, созданную из указанной последовательности кодовых единиц UTF-16
   return `https://img.icons8.com/ios-filled/1x/marker-${letter}.png`;
   // https://icons8.com/icons/set/pure
 });
 
 export const YMap = (props) => (
   <YMaps query={{ load: "package.full" }}>
+  {/* пакет Ymaps по умолчанию */}
     <Map
+    // Класс для создания и управления картой
       state={{
+        // Параметры карты.
         center,
+        // геокоординаты
         zoom: 15,
+        // масштабирование карты
         controls: []
+        // Элементы управления картой.
       }}
       width="100vw"
       height="100vh"
@@ -25,7 +34,7 @@ export const YMap = (props) => (
           geometry={center.map((c) => c + (Math.random() - 0.6))}
           options={{
             iconLayout: "default#image",
-            iconImageSize: [50, 50],
+            iconImageSize: [50, 50],//размер иконки
             iconImageHref: n
           }}
         />
